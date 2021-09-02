da743e44c0a7279ec90f28d0f34ff93e86ec15c9
Terrence Rodgers <tdr0082@gmail.com>
2021-09-01 22:57:49 -0500

    Initial Commit

---
+++ app/src/main/java/com/tdr/app/kimikoscanvas/fragments/AboutFragment.kt
@@ -0,0 +1,28 @@
+package com.tdr.app.kimikoscanvas.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.tdr.app.kimikoscanvas.R
+import com.tdr.app.kimikoscanvas.databinding.AboutFragmentBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [AboutFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class AboutFragment : Fragment() {
+
+    private lateinit var binding : AboutFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.about_fragment, container, false)
+    }
+
+}
