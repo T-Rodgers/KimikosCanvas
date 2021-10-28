61721a7349360277e8254f5aaf4560f5f12422db
Terrence Rodgers <tdr0082@gmail.com>
2021-10-28 15:17:50 -0500

    Removed unused methods and TODOS

--- app/src/main/java/com/tdr/app/kimikoscanvas/canvas/CanvasViewModel.kt
+++ app/src/main/java/com/tdr/app/kimikoscanvas/canvas/CanvasViewModel.kt
@@ -1,11 +1,9 @@
 package com.tdr.app.kimikoscanvas.canvas
 
-import androidx.core.net.toUri
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.google.firebase.storage.StorageReference
 import com.tdr.app.kimikoscanvas.utils.FirebaseUtils
 import kotlinx.coroutines.launch
 import timber.log.Timber
@@ -19,7 +17,6 @@ class CanvasViewModel() : ViewModel() {
     val canvases: LiveData<List<Canvas>>
         get() = _canvases
 
-    // TODO: (3) Implement navigation for when image is clicked on
     private val _navigateToDetails = MutableLiveData<Boolean>()
     val navigateToDetails: LiveData<Boolean>
         get() = _navigateToDetails
@@ -43,23 +40,14 @@ class CanvasViewModel() : ViewModel() {
         viewModelScope.launch {
             FirebaseUtils().retrieveProducts(object : FirebaseUtils.FirebaseServiceCallback{
                 override fun onImageCallback(value: String) {
-                    TODO("Not yet implemented")
+                    // For future implementation of ImageViewing
                 }
 
                 override fun onProductListCallback(value: List<Canvas>) {
                     _canvases.value = value
                     Timber.i("${value.size}")
                 }
-
             })
         }
     }
-
-    // TODO: (1) Link realtime database and storage
-//    private fun retrieveCanvasesFromDatabase(){
-//        viewModelScope.launch {
-//            val canvasList = mutableListOf<Canvas>()
-//        }
-//    }
-
 }
