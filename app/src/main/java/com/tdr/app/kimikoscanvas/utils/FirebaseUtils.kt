16fd9d154ca8c1163665a5440e85587c2c267a5d
Terrence Rodgers <tdr0082@gmail.com>
2021-10-28 15:17:21 -0500

    Add comments to firebase utils code

--- app/src/main/java/com/tdr/app/kimikoscanvas/utils/FirebaseUtils.kt
+++ app/src/main/java/com/tdr/app/kimikoscanvas/utils/FirebaseUtils.kt
@@ -1,6 +1,9 @@
 package com.tdr.app.kimikoscanvas.utils
 
-import com.google.firebase.database.*
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
@@ -17,6 +20,9 @@ class FirebaseUtils {
 //        storage.reference.child(IMAGES_PATH)
 //    private val productRef = database.reference.child(PRODUCTS_PATH)
 
+    /**
+     * Retrieves list of canvases from database reference.
+     */
     fun retrieveProducts(callback: FirebaseServiceCallback) {
         val canvasList = mutableListOf<Canvas>()
         database.reference.child(PRODUCTS_PATH)
@@ -36,10 +42,19 @@ class FirebaseUtils {
         Timber.i("${canvasList.size}")
     }
 
+
+    /**
+     * Closes even listener that was used to retrieve any changes to database
+     */
     fun removeListener(database: FirebaseDatabase, listener: ValueEventListener) {
         database.reference.child(PRODUCTS_PATH).removeEventListener(listener)
     }
 
+
+    /**
+     * Call back that waits until products are added to canvasList. Used to retrieve values and
+     * use them in our viewModel
+     */
     interface FirebaseServiceCallback {
         fun onImageCallback(value: String)
         fun onProductListCallback(value: List<Canvas>)
