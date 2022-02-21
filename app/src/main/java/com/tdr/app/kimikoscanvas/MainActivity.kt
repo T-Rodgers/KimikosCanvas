b77ae78fb1b172ca67e7babba4c391f770c38c30
Terrence Rodgers <tdr0082@gmail.com>
2022-02-21 12:50:18 -0600

    Remove drawerlayout implementation

--- app/src/main/java/com/tdr/app/kimikoscanvas/MainActivity.kt
+++ app/src/main/java/com/tdr/app/kimikoscanvas/MainActivity.kt
@@ -3,10 +3,6 @@ package com.tdr.app.kimikoscanvas
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.NavController
-import androidx.navigation.NavDestination
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
@@ -15,7 +11,6 @@ import com.tdr.app.kimikoscanvas.databinding.ActivityMainBinding
 import com.tdr.app.kimikoscanvas.utils.FirebaseUtils
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var drawerLayout: DrawerLayout
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
@@ -31,24 +26,14 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        drawerLayout = binding.drawerLayout
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         val navController = navHostFragment.navController
-        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupActionBarWithNavController(this, navController)
         appBarConfiguration =
-            AppBarConfiguration(setOf(R.id.listFragment, R.id.loginFragment), drawerLayout)
+            AppBarConfiguration(setOf(R.id.listFragment))
 
         binding.toolbar.setupWithNavController(navController, appBarConfiguration)
-        NavigationUI.setupWithNavController(binding.navView, navController)
-
-        navController.addOnDestinationChangedListener { _: NavController, destination: NavDestination, _: Bundle? ->
-            if (destination.id == navController.graph.startDestination) {
-                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
-            } else {
-                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
-            }
-        }
 
     }
 
