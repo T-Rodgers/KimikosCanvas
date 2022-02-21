c29a7c4197916837b75f3e59e9820e0a235086d8
Terrence Rodgers <tdr0082@gmail.com>
2022-02-21 12:40:11 -0600

    Initial Commit

---
+++ app/src/androidTest/java/com/tdr/app/kimikoscanvas/fragments/ListFragmentTest.kt
@@ -0,0 +1,14 @@
+package com.tdr.app.kimikoscanvas.fragments
+
+import androidx.test.filters.MediumTest
+import org.junit.Test
+
+@MediumTest
+class ListFragmentTest {
+
+    @Test
+    fun clickItemInList_navigateToDetails() {
+
+
+    }
+}
